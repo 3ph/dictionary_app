e98c52c02e3c4e0d02f6bdaec373869ee66fd6ca
Tom Friml <instantni.med@gmail.com>
2023-11-11 21:45:14 +1300

    Removes old template code

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,4 @@
-import 'package:dictionary_app/app/app.dart';
+import 'package:dictionary_app/app.dart';
 import 'package:dictionary_app/bootstrap.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
