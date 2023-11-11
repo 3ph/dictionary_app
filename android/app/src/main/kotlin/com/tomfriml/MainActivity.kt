10afb6b71c02e0f8aaa133f3f14778935965fbc6
Tom Friml <instantni.med@gmail.com>
2023-11-11 21:17:25 +1300

    Initial template commit

---
+++ android/app/src/main/kotlin/com/tomfriml/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tomfriml.dictionary_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:dictionary_app/app/app.dart';
+import 'package:dictionary_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
